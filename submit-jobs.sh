6659439cde25d55aaf57172b83fc2916e37848b2
John-Paul Robinson <jpr@uab.edu>
2022-01-08 06:18:48 -0600

    Reformat job submit lines for easier read of sbatch args and add miscargs parameter

--- submit-jobs.sh
+++ submit-jobs.sh
@@ -4,6 +4,7 @@
 sizes=$1
 methods=$2
 challenges=$3
+miscargs=$4
 
 for size in $sizes
 do
@@ -11,7 +12,14 @@ do
   do
     for chall in $challenges
     do
-      NB=$notebook DATASET=mympd-full-${size}k CNAME=$chall && NB=$NB DATASET=$DATASET CNAME=$CNAME sbatch --job-name=$NB-$CNAME-$DATASET --mem=200G -n 4 -N 1 --partition=amd-hdr100 run-nb.sbatch                                                     
+      NB=$notebook DATASET=mympd-full-${size}k CNAME=$chall && \
+      NB=$NB DATASET=$DATASET CNAME=$CNAME \
+      sbatch --job-name=$NB-$CNAME-$DATASET \
+        -n 4 -N 1 \
+        --time=120 \
+        --mem=400G\
+        --partition=amd-hdr100 $miscargs \
+        run-nb.sh
       sleep 1
     done
   done
